470641878c5959c8da3d768da50d910ce234bc54
montyz <sonawanemanthan3@gmail.com>
2025-06-17 16:57:49 +0530

    added env variables according to vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,17 @@ import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
+import vercel from 'vite-plugin-vercel';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills(),tailwindcss()],
+  plugins: [react(), nodePolyfills(),tailwindcss(), vercel()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  define : {
+    __APP_ENV__: process.env.VITE_VERCEL_ENV,
+  }
 })
